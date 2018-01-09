7b2c136932dbc849ca665d45cf2aa21b2372f053
HBN <ngohoangbao@outlook.com>
2018-01-10 00:08:15 +0100

    ddd

--- src/index.js
+++ src/index.js
@@ -3,11 +3,46 @@ import ReactDOM from 'react-dom';
 import "./css/index.css";
 import registerServiceWorker from './js/registerServiceWorker';
 
+
+
 class App extends React.Component {
+    state = {list: []};
+
+    loadData() {
+        // fetch("https://fcctop100.herokuapp.com/api/fccusers/top/recent") 
+        fetch("https://randomuser.me/api/?results=100")
+        .then((resp) => resp.json())
+        .then(data => {
+            var array = data;
+            console.log(array);
+            this.setState({list: array.results});
+            // console.log(array.results[0]);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
     render(){
+        const myList = this.state.list.map((person) => 
+            <div key={person.email}>
+                <div>Image</div>
+                <div>Name</div>
+                <div>Points</div>
+                <div>Alltimes</div>
+            </div>
+            
+        );
         return (
             <div>
-                "hello world"
+                <h1 className="title">Top 100 in the last 30 days</h1>
+                <div id="brownies">
+                    {myList}
+                </div>
             </div>
         );
     }
