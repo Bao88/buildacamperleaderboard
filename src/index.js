b5c19a056301ca594e15b9e92d8b8172dc6a5778
Bao88 <bao88@live.no>
2018-01-10 11:24:23 +0100

    almost finished

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import registerServiceWorker from './js/registerServiceWorker';
 
 
 
-class App extends React.Component {
-    state = {list: []};
-
-    loadData() {
-        // fetch("https://fcctop100.herokuapp.com/api/fccusers/top/recent") 
-        fetch("https://randomuser.me/api/?results=100")
+class Data extends React.Component {
+     state = {list: []};
+    loadData(which) {
+        // console.log("called");
+        var url = which ? "https://fcctop100.herokuapp.com/api/fccusers/top/recent":"https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
+        fetch(url) 
+        // fetch("https://randomuser.me/api/?results=100")
         .then((resp) => resp.json())
         .then(data => {
             var array = data;
             console.log(array);
-            this.setState({list: array.results});
+            this.setState({list: array});
             // console.log(array.results[0]);
         })
         .catch(function(error){
@@ -24,24 +25,55 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.loadData();
+        this.loadData(true);
     }
 
-    render(){
-        const myList = this.state.list.map((person) => 
-            <div key={person.email}>
-                <div>Image</div>
-                <div>Name</div>
-                <div>Points</div>
-                <div>Alltimes</div>
+    render() {
+        const myList = this.state.list.map((person, index) => 
+            <div key={person.username}>
+                <span>{index+1}</span>
+                <img src={person.img} alt={person.img}/>
+                <span>{person.username}</span>
+                <span>{person.recent}</span>
+                <span>{person.alltime}</span>
             </div>
-            
         );
+        return myList
+    }
+}
+
+class App extends React.Component {
+    state = {bool: true};
+
+    loadRecent = () => {
+        this.setState({bool: true});
+        console.log("load recent");
+    }
+
+    loadAlltime = () => {
+        this.setState({bool: false});
+        console.log("load alltime");
+    }
+    render(){
+        const titles = {
+            width: "580px"
+        };
+        const click = {
+            cursor: "pointer"
+        }
+
         return (
             <div>
                 <h1 className="title">Top 100 in the last 30 days</h1>
                 <div id="brownies">
-                    {myList}
+                    <div className="resultTitle">
+                        <span>#</span>
+                        <div></div>
+                        <span style={titles}>Camper name</span>
+                        <span onClick={() => {this.data.loadData(true)}} style={click}>Recent</span>
+                        <span onClick={() => {this.data.loadData(false)}} style={click}>Alltime</span>
+                    </div>
+                    <Data ref={instance =>  {this.data = instance;}}/>
                 </div>
             </div>
         );
